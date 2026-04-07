2943465b1564fb3e878ef939241f347cf0fb96de
sara faltas <gadly@cloudconceptgroup.com>
2026-04-07 15:33:30 +0200

    updated

--- src/question.js
+++ src/question.js
@@ -1,7 +1,26 @@
 class Question {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (text, choices, answer, difficulty)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-    // 2. shuffleChoices()
+  // 2. shuffleChoices()
+  // should shuffle the elements stored in the choices array property.
+  shuffleChoices() {
+      for (let i = this.choices.length - 1; i > 0; i--) {
+        // Pick a random index from 0 to i
+        const randomIndex = Math.floor(Math.random() * (i + 1));
+        // Swap elements
+        [this.choices[i],this.choices[randomIndex]] = [this.choices[randomIndex],this.choices[i]];
+      
+    }
+
+      return choices;
+    
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -2,14 +2,49 @@ class Quiz {
     // YOUR CODE HERE:
     //
     // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining){
+        this.questions = questions
+        this.timeLimit = timeLimit
+        this.timeRemaining = timeRemaining
+        this.correctAnswers = 0
+        this.currentQuestionIndex = 0
+    }
 
     // 2. getQuestion()
+    // should return the item from the questions array at the position of currentQuestionIndex
+    getQuestion(){
+  
+        
+    }
     
     // 3. moveToNextQuestion()
+   //should increment the currentQuestionIndexby1
+    moveToNextQuestion(){
+      
+
+    }
 
     // 4. shuffleQuestions()
+    //should shuffle the items in the questions array.
+    shuffleQuestions(){
+     
+    }
 
     // 5. checkAnswer(answer)
+    //should increase correctAnswers by 1 when called with a correct answer for the current question
+    checkAnswer(answer){
+    
+    }
 
     // 6. hasEnded()
+    //should return false when currentQuestionIndex is less than the questions array length
+    //should return true when currentQuestionIndex is equal to the questions array length
+    hasEnded(){
+   
+    }
+
+    averageDifficulty(){
+
+
+    }
 }
